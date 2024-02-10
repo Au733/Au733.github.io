155778a711d969e068e47f615eaa630962330fad
Kou Lee <200148981@fvtc.edu>
2024-02-10 00:06:12 -0600

    Added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,23 @@
-alert('hi')
+//alert('hi')
+
+const hours = new Date().getHours() // get the current hour
+
+/*const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?*/
+
+function greeting (hours){
+    if (hours >= 4 && hours < 12) {
+        return "Good Morning"
+    }
+    else if (hours >= 12 && hours < 17) {
+        return "Good Afternoon"
+    }
+    else if (hours >= 17 || hours < 4) {
+        return "Good Evening"
+    }
+
+}
+
+const welcomeMessage = document.querySelector('#welcome')
+welcomeMessage.textContent = greeting(hours)
